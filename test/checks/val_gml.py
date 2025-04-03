78c5264b905bb832a29c167e9aeb2942e8e00cf5
Jerome H. Kaempf <jerome.kaempf@kaem.co>
2025-04-03 18:15:47 +0200

    Patch of val_gml.py to add manually a missing schema in cityGMLBase.xsd

--- test/checks/val_gml.py
+++ test/checks/val_gml.py
@@ -17,11 +17,21 @@ case = "600_profiles"
 
 schema = requests.get("http://www.sig3d.org/citygml/2.0/energy/1.0/EnergyADE.xsd").content
 
-doc = etree.parse(os.path.join(SIMULATIONS_DIR,case,case+".gml"))
+lines = schema.split(b"\n")
+index = 0
+for idx, line in enumerate(lines):
+    if line.strip().startswith(b'<import'):
+        index = idx
+        break
+
+lines.insert(index+1, b'<import namespace="http://www.opengis.net/citygml/xAL/2.0" schemaLocation="http://schemas.opengis.net/citygml/xAL/xAL.xsd" />')
+schema = b"\n".join(lines)
 
 xsd = etree.fromstring(schema)
 
 xmlschema = etree.XMLSchema(xsd)
+
+doc = etree.parse(os.path.join(SIMULATIONS_DIR,case,case+".gml"))
 valid = doc.xmlschema(xsd)
 
 if valid:
@@ -29,4 +39,3 @@ if valid:
 else:
     print("Invalid document.")
     xmlschema.assert_(doc)
-
